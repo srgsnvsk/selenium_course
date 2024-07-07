f5857f8c9e0f83e47c3e3709059ca6f9607c24a9
srgsnvsk <srgsnvsk@gmail.com>
2024-07-07 22:29:52 +0300

    Еще один пройденный урок

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,12 +1,12 @@
-import pytest
+# import pytest
 from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()  
+    page.solve_quiz_and_get_code()
     # page.should_be_success_message()
     # page.should_be_correct_product_name()
     # page.should_be_correct_product_price()
